6768a9a792352c7415be05838af84d3a1a674665
AlexViti <alexviti12@gmail.com>
2022-03-01 14:20:40 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const container = document.getElementById('main-container');
+const output = document.querySelector('.output');
+const nav = document.querySelector('nav');
+const footer = document.querySelector('footer');
